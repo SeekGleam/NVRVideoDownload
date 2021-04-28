0d315ef510a7a63802f345b849b85a34dd8f539a
zhangbo <1614289100@qq.com>
2021-04-28 21:56:27 +0800

    新增底层海康NVR\CVR\大华NVR视频下载

---
+++ src/main/java/com/nvr/video/domain/vo/TaskVO.java
@@ -0,0 +1,44 @@
+package com.nvr.video.domain.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author zhangbo
+ * @date 2021/4/28 16:10
+ * @description
+ */
+@Data
+@Builder
+@ApiModel(value = "TaskVO" ,description = "下载任务展示数据")
+public class TaskVO {
+
+    @ApiModelProperty(name = "任务ID",example = "")
+    private String taskId;
+
+    @ApiModelProperty(name = "下载进度名称",example = "89%")
+    private String downloadProgressName;
+
+    @ApiModelProperty(name = "下载进度",example = "89")
+    private Integer downloadProgressNum;
+
+    @ApiModelProperty(name = "下载状态名称",example = "准备下载.....")
+    private String downloadStatusName;
+
+    @ApiModelProperty(name = "下载状态代码",example = "0")
+    private String downloadStatusCode;
+
+    @ApiModelProperty(name = "文件名称",example = "0")
+    private String videoFileName;
+
+    @ApiModelProperty(value = "视频截取开始时间" ,example = "2020-09-10 13:00:00")
+    private Date clipStartTime;
+
+    @ApiModelProperty(value = "视频截取结束时间" ,example = "2020-09-10 15:00:00")
+    private Date clipEndTime;
+
+}
