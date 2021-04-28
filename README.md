35e0f8c1d77e4697849f3694213fa1ebe59a771d
zhangbo <1614289100@qq.com>
2021-04-28 15:22:52 +0800

    提交初始版本ReadMe,去除不必要的状态码

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# NVRVideoDownload
-海康NVR、CVR，大华NVR视频下载
+提供海康威视NVR、CVR，浙江大华NVR按时间段进行视频截取下载功能。
+
+
+SDK环境准备：
+
+| **设备** | **操作系统** | **SDK版本** | **安装说明** | **备注** |
+| :---: | :---: | :---: | :---: | :---: |
+| 海康威视 | windows_x64 | [设备网络SDK_V6.1.6.4(for Windows x64)](https://www.hikvision.com/cn/download_more_570.html)
+| 1.指定**HCNETSDK_PATH为**HCNetSDK.dll路径。
+2.指定**PLAYCTRL_PATH为**PlayCtrl.dll路径 | 支持海康威视NVR、CVR视频下载。 |
+|  | linux_x64 | [设备网络SDK_V6.1.6.4(for Linux64)](https://www.hikvision.com/cn/download_more_403.html) |  |  |
+| 浙江大华 | windows_x64 | [设备网络SDK_JAVA_Win64_V3.052.0000002.0.R.201103]() | 1.指定**DH_LIB_PATH为sdk根目录即可** | 此版本只支持大华私有文件格式（dav），mp4下载格式可联系大华客服提供。 |
+|  | linux_x64 | [设备网SDK_JAVA_Linux64_V3.052.0000002.0.R.201103](https://www.dahuatech.com/service/downloadlists/836.html) |  |  |
+
+NVRVideoDownload程序提供以下接口：
+
+| **序号** | **接口名称** | **接口说明** |
+| :---: | :---: | :---: |
+| 1 | 按时间段截取海康NVR视频文件 |  |
+| 2 | 按时间段截取海康CVR视频文件 |
+|
+| 3 | 按时间段截取大华NVR视频文件 |
+|
+| 4 | 获取提交任务下载状态 |
+|
--- src/main/java/com/nvr/video/exception/CommonException.java
+++ src/main/java/com/nvr/video/exception/CommonException.java
@@ -11,9 +11,6 @@ public class CommonException {
      * ************************************** 用户异常 **************************************
      */
     public static final BusinessException ERROR_REQUEST = new BusinessException("A0001", "无效的请求");
-    public static final BusinessException ERROR_SYNTHESIS_VIDEO_REQUEST = new BusinessException("A0005", "视频合成失败，原因是多合一标志与实际视频文件个数不一致");
-    public static final BusinessException REQUEST_CAMERA_CODE_ERROR = new BusinessException("A0010", "视频编码参数传入错误");
-    public static final BusinessException CUSTOM_TIME_DOWNLOAD_PARAM_ERROR = new BusinessException("A0015", "自定义计时下载参数错误");
     public static final BusinessException REQUEST_TIME_ERROR = new BusinessException("A0020", "传入时间有误");
     public static final BusinessException REQUEST_TIME_INVALID = new BusinessException("A0025", "时间参数有误，开始时间大于或等于结束时间");
 
@@ -22,12 +19,10 @@ public class CommonException {
      */
     public static final BusinessException SYS_ERROR = new BusinessException("B0001", "系统内部异常");
     public static final BusinessException SINGLE_VIDEO_PROCESS_FAILED = new BusinessException("B0005", "单个视频处理失败");
-    public static final BusinessException FINAL_VIDEO_PROCESS_FAILED = new BusinessException("B0010", "合成最终视频失败");
     public static final BusinessException DOWNLOAD_VIDEO_SIZE_0 = new BusinessException("B0015", "下载的原始视频为0字节");
     public static final BusinessException DOWNLOAD_VIDEO_FAILED = new BusinessException("B0020", "视频下载失败");
     public static final BusinessException DOWNLOAD_VIDEO_PIECEWISE_MERGING_FAILED = new BusinessException("B0020", "视频下载分片合并处理失败");
     public static final BusinessException ENVIRONMENT_CONFIG_LOAD_FAILED = new BusinessException("B0025", "环境配置读取失败");
-    public static final BusinessException GPU_NUM_CONFIG_ERROR = new BusinessException("B0030", "GPU核数配置有误");
     public static final BusinessException SDK_INIT_ERROR = new BusinessException("B0035", "海康SDK初始化失败");
 
     /**
